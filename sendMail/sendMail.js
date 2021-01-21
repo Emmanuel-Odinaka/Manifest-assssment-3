14d37d0ec329ca729c28d53d8f1d08d2043e5961
Veralee225 <adielevera226@gmail.com>
2021-01-21 12:59:05 +0000

    replaced deleted files

---
+++ sendMail/sendMail.js
@@ -0,0 +1,16 @@
+function sendMailById() {
+	let evenBatch = []
+	let oddBatch = []
+    for (let i=1; i<=100; i++) {
+		if (i % 2 === 0) {
+			evenBatch.push(i)
+		} else {
+			oddBatch.push(i)
+		}
+	} 
+	console.log(evenBatch)
+	console.log(oddBatch)
+	console.log(`There are ${evenBatch.length} even batch User ID`)
+	console.log(`There are ${oddBatch.length} odd batch User ID`)
+}
+sendMailById()
